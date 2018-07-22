e5e5d3261f8efded955e59e971c1281e37bd6535
yichiang <yi_chiang@Outlook.com>
2018-07-21 20:14:16 -0700

    attempt to use

--- app/src/main/java/com/example/android/android_me/ui/StepDetailFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/StepDetailFragment.java
@@ -16,9 +16,17 @@
 
 package com.example.android.android_me.ui;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.media.session.MediaButtonReceiver;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +37,27 @@ import android.widget.TextView;
 
 import com.example.android.android_me.R;
 import com.example.android.android_me.model.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.util.ArrayList;
 
@@ -38,6 +67,18 @@ import java.util.ArrayList;
 public class StepDetailFragment extends Fragment {
     public static final String STEP_ID_LIST = "steps_ids";
     public static final String LIST_INDEX = "list_index";
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
+
+    private Timeline.Window window;
+    private DataSource.Factory mediaDataSourceFactory;
+    private DefaultTrackSelector trackSelector;
+    private boolean shouldAutoPlay;
+    private BandwidthMeter bandwidthMeter;
+    View rootView;
+//    private PlaybackStateCompat.Builder mStateBuilder;
+
+//    private static MediaSessionCompat mMediaSession;
 
     ArrayList<Step> mSteps;
     Integer mListIndex = 0;
@@ -70,7 +111,7 @@ public class StepDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        final View rootView = inflater.inflate(R.layout.fragment_detail_step, container, false);
+        rootView = inflater.inflate(R.layout.fragment_detail_step, container, false);
 
 
         if(savedInstanceState != null) {
@@ -79,18 +120,99 @@ public class StepDetailFragment extends Fragment {
         }
         TextView descTextView = rootView.findViewById(R.id.receipt_step_description);
         descTextView.setText(mSteps.get(mListIndex).getDescription());
-        StepDetailActivity activity = (StepDetailActivity) getActivity();
+//        StepDetailActivity activity = (StepDetailActivity) getActivity();
 
+        mPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.playerView);
+        mPlayerView.requestFocus();
 
-
+        // Initialize the player.
+        initializePlayer(Uri.parse(mSteps.get(mListIndex).getVideoURL()));
 
         return rootView;
     }
 
+    /**
+     Source:  https://raw.githubusercontent.com/yusufcakmak/ExoPlayerSample/master/app/src/main/java/com/yusufcakmak/exoplayersample/VideoPlayerActivity.java
+     */
+    private void initializePlayer(Uri uri) {
+
+        mPlayerView = (SimpleExoPlayerView) rootView.findViewById(R.id.playerView);
+        mPlayerView.requestFocus();
+
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+
+        trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        LoadControl loadControl = new DefaultLoadControl();
+
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(this.getContext(), trackSelector, loadControl);
+
+        mPlayerView.setPlayer(mExoPlayer);
+
+        mExoPlayer.setPlayWhenReady(shouldAutoPlay);
+/*        MediaSource mediaSource = new HlsMediaSource(Uri.parse("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"),
+                mediaDataSourceFactory, mainHandler, null);*/
+
+        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        MediaSource mediaSource = new ExtractorMediaSource(uri,
+                mediaDataSourceFactory, extractorsFactory, null, null);
+
+        mExoPlayer.prepare(mediaSource);
+
+//        ivHideControllerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mPlayerView.hideController();
+//            }
+//        });
+    }
+
+    private void releasePlayer() {
+        if (mExoPlayer != null) {
+            shouldAutoPlay = mExoPlayer.getPlayWhenReady();
+            mExoPlayer.release();
+            mExoPlayer = null;
+            trackSelector = null;
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer(Uri.parse(mSteps.get(mListIndex).getVideoURL()));
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || mExoPlayer == null)) {
+            initializePlayer(Uri.parse(mSteps.get(mListIndex).getVideoURL()));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
     @Override
     public void onSaveInstanceState(Bundle currentState) {
         currentState.putSerializable(STEP_ID_LIST, mSteps);
         currentState.putInt(LIST_INDEX, mListIndex);
     }
+
 
 }
